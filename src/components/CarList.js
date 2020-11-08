cc2845869fe7d1d789a6614a348f255159edef74
sergiogomes <sergiopgomes92@gmail.com>
2020-11-08 00:43:53 -0300

    Calling car's API and showing results.

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,12 +1,45 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, Text, Image} from 'react-native';
+import Axios from 'axios';
 
 const CarList = () => {
+  const [carList, setCarList] = useState([]);
+
+  const {imageStyle} = styles;
+
+  useEffect(() => {
+    Axios.get('https://givecars.herokuapp.com').then((resp) => {
+      setCarList(resp.data);
+    });
+  }, []);
+
   return (
     <View>
       <Text>Car List</Text>
+      <View>
+        {carList.map((car) => (
+          <View>
+            {car.model.map((model) => (
+              <View>
+                <Text>
+                  {car.brand} {model.name}
+                </Text>
+                <Text>{model.url}</Text>
+                <Image style={imageStyle} source={{uri: model.image}} />
+              </View>
+            ))}
+          </View>
+        ))}
+      </View>
     </View>
   );
+};
+
+const styles = {
+  imageStyle: {
+    height: 200,
+    width: 500,
+  },
 };
 
 export default CarList;
