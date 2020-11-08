f45af07a76537dc51e98ce25180aea6336f2fd7e
sergiogomes <sergiopgomes92@gmail.com>
2020-11-08 20:21:50 -0300

    Wrapping up.

--- src/components/CarDetail.js
+++ src/components/CarDetail.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import {View, Text, Image} from 'react-native';
+import {View, Text, Image, Linking} from 'react-native';
 
 import Item from './Item';
+import Button from './Button';
 import ItemSection from './ItemSection';
 
 const CarDetail = ({cars}) => {
@@ -16,15 +17,20 @@ const CarDetail = ({cars}) => {
               <Item key={model.name}>
                 <ItemSection>
                   <View style={headerContainer}>
-                    <Text style={headerText}>{car.brand}</Text>
-                    <Text style={headerText}>{model.name}</Text>
+                    <Text style={headerText}>
+                      {car.brand} - {model.name}
+                    </Text>
                   </View>
                 </ItemSection>
                 <ItemSection>
-                  <Text>{model.url}</Text>
+                  <Image style={imageStyle} source={{uri: model.image}} />
                 </ItemSection>
                 <ItemSection>
-                  <Image style={imageStyle} source={{uri: model.image}} />
+                  <Button
+                    buttonPress={buttonPress}
+                    url={model.url}
+                    title={'Click me'}
+                  />
                 </ItemSection>
               </Item>
             ))}
@@ -37,11 +43,16 @@ const CarDetail = ({cars}) => {
   return renderList();
 };
 
+const buttonPress = (url) => {
+  Linking.openURL(url);
+};
+
 const styles = {
   imageStyle: {
     height: 200,
     flex: 1,
     width: 0,
+    borderRadius: 4,
   },
   headerContainer: {
     flexDirection: 'column',
